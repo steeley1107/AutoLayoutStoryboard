163d16d32af6494801cf0bc1c03dc447ce5f780c
Jay Steele <steeley1107@gmail.com>
2015-10-27 14:15:14 -0700

    firstCommit

---
+++ AutoLayoutStoryboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayoutStoryboard
+//
+//  Created by Steele on 2015-10-27.
+//  Copyright © 2015 Steele. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
